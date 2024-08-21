322446700eae5b7d201e4c54d7ecea6da6757c66
cisco jnr <ciscojnr361@gmail.com>
2024-08-21 18:49:21 +0000

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+function NotFound() {
+    return (
+        <div className="container text-center">
+            <div className="row">
+                <div className="col">
+                    <h1 className="display-1">404</h1>
+                    <p className="lead">Oops! The page you are looking for does not exist.</p>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}    
+
+export default NotFound
